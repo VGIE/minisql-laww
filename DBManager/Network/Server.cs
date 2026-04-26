b724cf51854dfcfe146a7b9d1f6484c658fe7fac
arago <aragonest@outlook.com>
2026-04-26 12:22:54 +0200

    #66 Server class implemented, server up and working

--- DBManager/Network/Server.cs
+++ DBManager/Network/Server.cs
@@ -18,7 +18,58 @@ namespace DbManager.Network
             //DEADLINE 6: Implement the server as specified (eGela)
             //Have a look at the project ServerConsole to see how a TcpListener is used
             //Use XmlSerializer to create Xml commands
-            
-        }        
+
+            try
+            {
+                //Crear server
+
+                // llamar a listen (puerto) en server
+
+
+                DbManager.Database serverDatabase = new Database("admin", "adminPassword");
+                //Listen on port 1200. Accept connections from any IP address
+
+                TcpListener server = new TcpListener(IPAddress.Parse("0.0.0.0"), port);
+
+                server.Start();
+
+                Console.WriteLine("Server running and listening on port 1200");
+
+                Socket socket = server.AcceptSocket();
+
+                Console.WriteLine("Connection accepted from " + socket.RemoteEndPoint);
+
+                bool trueFalse = true;
+                while (trueFalse == true)
+                {
+                    byte[] buffer = new byte[100];
+                    int bytesRead = socket.Receive(buffer);
+                    buffer[bytesRead] = 0;
+                    ASCIIEncoding encoding = new ASCIIEncoding();
+                    string clientMessage = encoding.GetString(buffer).Substring(0, bytesRead);
+                    Console.WriteLine("Message received from client: " + clientMessage);
+                    if (clientMessage == "Exit")
+                    {
+                        trueFalse = false;
+                    }
+                    else
+                    {
+                        string clientResult = serverDatabase.ExecuteMiniSQLQuery(clientMessage);
+                        socket.Send(encoding.GetBytes(clientResult));
+                    }
+                }
+
+                Task.Delay(2000).Wait();
+
+                socket.Close();
+                server.Stop();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unhandled error: " + e);
+            }
+
+        }  
     }
 }
--- Samples/ServerConsole/Program.cs
+++ Samples/ServerConsole/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using DbManager;
+using DbManager.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-using DbManager;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ServerConsole
 {
@@ -13,52 +14,13 @@ namespace ServerConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                DbManager.Database serverDatabase = new Database("admin", "adminPassword");
-                //Listen on port 1200. Accept connections from any IP address
-                TcpListener server = new TcpListener(IPAddress.Parse("0.0.0.0"), 1200);
-
-                server.Start();
-
-                Console.WriteLine("Server running and listening on port 1200");
-
-                Socket socket = server.AcceptSocket();
-
-                Console.WriteLine("Connection accepted from " + socket.RemoteEndPoint);
-
-                bool trueFalse = true;
-                while(trueFalse == true)
-                {
-                    byte[] buffer = new byte[100];
-                    int bytesRead = socket.Receive(buffer);
-                    buffer[bytesRead] = 0;
-                    ASCIIEncoding encoding = new ASCIIEncoding();
-                    string clientMessage = encoding.GetString(buffer).Substring(0,bytesRead);
-                    Console.WriteLine("Message received from client: " + clientMessage);
-                    if (clientMessage == "Exit")
-                    {
-                        trueFalse = false;
-                    }
-                    else
-                    {                      
-                        string clientResult = serverDatabase.ExecuteMiniSQLQuery(clientMessage);
-                        socket.Send(encoding.GetBytes(clientResult));
-                    }                   
-                }
-                
-                Task.Delay(2000).Wait();
-
-                socket.Close();
-                server.Stop();
+            Server server = new Server();
+            server.Listen(1200);
+     
 
                 Console.WriteLine("Server closed. Press any key to finish...");
                 Console.ReadKey();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unhandled error: " + e);
-            }
+            
         }
     }
 }
